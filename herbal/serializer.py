7d667c622d86473896e7f2177b8968dc3120e00e
kelvin <carreuky>
2023-04-11 22:38:39 +0300

    models and serializer

---
+++ herbal/serializer.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from herbal.models import Herbal
+
+class HerbalSerializer(serializers.Serializer):
+    class Meta:
+        model= Herbal
+        field ="__all__"
---
+++ herbal/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from herbal.models import Herbal
+from herbal.serializer import HerbalSerializer
+from rest_framework.response import Response
+
+# Create your views here.
+
+class HerbalList(APIView):
+    def get(self,request):
+        herbals = Herbal.objects.all()
+        serializer= HerbalSerializer(herbals,many = True)
+        return Response(serializer.data)
